9c34afc66ce7ed5adbb91019ce03f8988590c248
Ilo <hei.iloniaina@gmail.com>
2025-12-17 14:04:08 +0300

    finished db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TYPE category AS ENUM ('VEGETABLE','ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
 
-CREATE Type dish_type AS ENUM ('STARTER', 'MAIN', 'DESSERT');
+CREATE Type dish_type AS ENUM ('START', 'MAIN', 'DESSERT');
 
 CREATE TABLE dish (
     id serial primary key ,
     name varchar(255) not null,
-    type dish_type not null
+    dish_type dish_type not null
 );
 
 CREATE TABLE Ingredient (
