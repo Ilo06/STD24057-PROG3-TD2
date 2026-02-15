bc172b7e5ad91a4b52922f92275c7767bac1bb7b
Ilo <hei.iloniaina@gmail.com>
2026-02-15 16:13:10 +0300

    feat: improved test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,9 @@ public class Main {
         Ingredient tomate = dataRetriever.findIngredientById(2);
 
         if (tomate.getStockValueAt(t).equals(dataRetriever.getStockValueAt(2, t))) {
-            System.out.println("okay");
+            System.out.println("getStockValueAt : okay");
         } else {
-            System.out.println("not okay");
+            System.out.println("getStockValueAt : not okay");
             System.out.println(tomate.getStockValueAt(t));
             System.out.println(dataRetriever.getStockValueAt(2, t));
         }
@@ -20,9 +20,9 @@ public class Main {
 
         Dish dish = dataRetriever.findDishById(1);
         if (dish.getDishCost().equals(dataRetriever.getDishCost(1))) {
-            System.out.println("okay");
+            System.out.println("getDishCost : okay");
         } else {
-            System.out.println("not okay");
+            System.out.println("getDishCost : not okay");
             System.out.println(dish.getDishCost());
             System.out.println(dataRetriever.getDishCost(1));
 
@@ -30,9 +30,9 @@ public class Main {
         }
 
         if (dish.getGrossMargin().equals(dataRetriever.getGrossMargin(1))){
-            System.out.println("okay");
+            System.out.println("getGrossMargin : okay");
         }else  {
-            System.out.println("not okay");
+            System.out.println("getGrossMargin : not okay");
             System.out.println(dish.getGrossMargin());
             System.out.println(dataRetriever.getGrossMargin(1));
         }
